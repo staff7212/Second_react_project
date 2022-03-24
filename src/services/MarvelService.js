6a43c2fcf39c56339056abf0d215261d45dc65d2
Saveliy <staff7212@gmail.com>
2022-03-24 15:22:16 +0300

    Loading spinner and error message

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,19 +1,24 @@
-
 class MarvelService {
   _apiBase = 'https://gateway.marvel.com:443/v1/public/';
   _apiKey = 'apikey=a98ec362bdd90ae8d5d093393fe03dd8'
 
   getResource = async (url) => {
     const res = await fetch(url);
-  
+
     if (!res.ok) {
       throw new Error(`Could not fetch ${url}, status ${res.status}`)
     }
-  
+
     return await res.json();
   };
 
   // валидация описания, можно и через тернарный, но ... подставляется всегда
+  // можно через CSS
+  // display: -webkit-box;
+  // -webkit-box-orient: vertical;
+  // -webkit-line-clamp: 5;
+  // overflow: hidden;
+  // height: 90px;
   validDescription = (desc) => {
     if (desc.length === 0) {
       return "There is no description for this person. We apologize to you. Go to Wiki."
@@ -37,6 +42,7 @@ class MarvelService {
   _transformCharacter = (char) => {
     return {
       name: char.name,
+      //description: char.description,
       //description: char.description ? `${char.description.substring(0, 200)}...` : "There is no description for this person",
       description: this.validDescription(char.description),
       thumbnail: `${char.thumbnail.path}.${char.thumbnail.extension}`,
