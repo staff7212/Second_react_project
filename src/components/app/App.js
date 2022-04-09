03efde206de01897a8aaeb7d80a21319608282c2
Saveliy <staff7212@gmail.com>
2022-04-09 16:42:29 +0300

    Animation charList and mini update

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,5 +1,7 @@
 import {lazy, Suspense} from 'react';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import { CSSTransition } from 'react-transition-group';
+
 
 import Spinner from '../spinner/Spinner'
 import AppHeader from "../appHeader/AppHeader";
--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,4 +1,5 @@
-import { useState, useEffect, useRef } from 'react'
+import { useState, useEffect, useRef } from 'react';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 import PropTypes from 'prop-types';
 
 
@@ -56,40 +57,44 @@ const CharList = (props) => {
   }
   
   const tranformArrayChars = (arrChars) => {
-    const chars = arrChars.map(( {name, thumbnail, id}, index ) => {
-      const imgStyle = {objectFit: `${thumbnail?.includes('image_not_available') || thumbnail?.includes('4c002e0305708') ? 'unset' : 'cover'}`};
-      
-      //const active = this.props.activeId === id;
-      //const clazz = active ? 'char__item_selected' : '';
-
-      //className={`char__item ${clazz}`}>
-
-      return (
-        <li 
-        onClick={() => {
-          props.getId(id)
-          focusItem(index)
-        }} 
-        onKeyDown={(e) => {
-          if (e.key === ' ' || e.key === 'Enter') {
-            e.preventDefault();
-            props.getId(id)
-            focusItem(index)
-          }
-        }}
-        key={id}
-        ref={el => itemRefs.current[index] = el}
-        tabIndex={'0'}
-        className={`char__item`}>
-          <img src={thumbnail} style={imgStyle} alt={`character ${name}`}/>
-          <div className="char__name">{name}</div>
-        </li>
-      );
-    });
+    
     return (
-      <ul className="char__grid">
-        {chars}
-      </ul>
+      <TransitionGroup component={'ul'} className="char__grid">
+        {arrChars.map(( {name, thumbnail, id}, index ) => {
+            const imgStyle = {objectFit: `${thumbnail?.includes('image_not_available') || thumbnail?.includes('4c002e0305708') ? 'unset' : 'cover'}`};
+            
+            //const active = this.props.activeId === id;
+            //const clazz = active ? 'char__item_selected' : '';
+      
+            //className={`char__item ${clazz}`}>
+      
+            return (
+              <CSSTransition
+                timeout={Math.round(350 + Math.random() * 1000)} 
+                key={id}
+                classNames='char__item'>
+                <li
+                  onClick={() => {
+                    props.getId(id)
+                    focusItem(index)
+                  }} 
+                  onKeyDown={(e) => {
+                    if (e.key === ' ' || e.key === 'Enter') {
+                      e.preventDefault();
+                      props.getId(id)
+                      focusItem(index)
+                    }
+                  }}
+                  ref={el => itemRefs.current[index] = el}
+                  tabIndex={'0'}
+                  className='char__item'>
+                    <img src={thumbnail} style={imgStyle} alt={`character ${name}`}/>
+                    <div className="char__name">{name}</div>
+                </li>
+              </CSSTransition>
+            );
+          })}
+      </TransitionGroup>
     )
   }
  
